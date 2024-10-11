740e03c378390461416ed7fc236871cd38311175
directrl <director@black-matter.org>
2024-10-11 23:20:29 +0200

    Configuration/GameSettings: Get, Set methods and SettingChange delagate

--- src/QuarkEngine.Configuration/GameSettings.cs
+++ src/QuarkEngine.Configuration/GameSettings.cs
@@ -1,12 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace QuarkEngine.Configuration {
+
+	public delegate void SettingChangeEvent(string key, object from, object to);
 	
 	public class GameSettings : IDisposable {
 		
 		private FileInfo ConfigFile { get; }
 		private JObject JsonConfig { get; }
+
+		public event SettingChangeEvent? SettingChange;
 
 		public GameSettings(FileInfo configFile) {
 			if(!configFile.Exists) {
@@ -27,6 +32,18 @@ namespace QuarkEngine.Configuration {
 			return defaultValue;
 		}
 		
+		public T Get<T>(string key) {
+			var value = JsonConfig[key];
+			if(value != null) return value.Value<T>();
+			return default;
+		}
+
+		public void Set<T>(string key, T value) {
+			var prevValue = JsonConfig[key];
+			if(prevValue != null) OnSettingChange(key, value, prevValue.Value<T>());
+			JsonConfig[key] = JToken.FromObject(value);
+		}
+		
 		public void Dispose() {
 			using(var writer = ConfigFile.CreateText())
 			using(var json = new JsonTextWriter(writer)) {
@@ -35,6 +52,10 @@ namespace QuarkEngine.Configuration {
 				
 				JsonConfig.WriteTo(json);
 			}
+		}
+
+		protected void OnSettingChange(string key, object from, object to) {
+			SettingChange?.Invoke(key, from, to);
 		}
 	}
 }
